fa68bbfc731e2b444874154c644ac2495b3b044f
AymenBida <aymen.bida.00@gmail.com>
2021-04-30 22:20:57 +0100

    Change webpack settings

--- webpack/prod.js
+++ webpack/prod.js
@@ -1,8 +1,10 @@
-const merge = require('webpack-merge');
 const TerserPlugin = require('terser-webpack-plugin');
-const base = require('./base');
+const webpack = require('webpack');
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
 
-module.exports = merge(base, {
+module.exports = {
   mode: 'production',
   output: {
     filename: 'bundle.min.js',
@@ -23,4 +25,45 @@ module.exports = merge(base, {
       }),
     ],
   },
-});
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+        },
+      },
+      {
+        test: [/\.vert$/, /\.frag$/],
+        use: 'raw-loader',
+      },
+      {
+        test: /\.(gif|png|jpe?g|svg|xml)$/i,
+        use: 'file-loader',
+      },
+      {
+        test: /\.m?js$/,
+        exclude: /(node_modules|bower_components)/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['@babel/preset-env'],
+          },
+        },
+      },
+    ],
+  },
+  plugins: [
+    new CleanWebpackPlugin({
+      root: path.resolve(__dirname, '../'),
+    }),
+    new webpack.DefinePlugin({
+      CANVAS_RENDERER: JSON.stringify(true),
+      WEBGL_RENDERER: JSON.stringify(true),
+    }),
+    new HtmlWebpackPlugin({
+      template: './index.html',
+    }),
+  ],
+};
